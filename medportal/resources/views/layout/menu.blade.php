245276ce9c5062a13838e790e9751100d5b9829c
mingburnu <program.developer@hotmail.com>
2017-01-20 10:49:16 +0800

    Signed-off-by: mingburnu <program.developer@hotmail.com>

--- medportal/resources/views/layout/menu.blade.php
+++ medportal/resources/views/layout/menu.blade.php
@@ -30,63 +30,65 @@
                                 {{ $title_i18n }}
                             @endif
                         </a>
-                        @foreach($page->children as $child)
-                            <ul>
-                                <?php
-                                $target_child = "";
-                                if (!$page->type) {
-                                    $target_child = "_blank";
-                                }
-                                ?>
-                                <a href="{{ $url = route('pages.id', ['id' => $page->id]) }}" target="{{$target_child}}">
-                                    @if(Cookie::get('language')==0)
-                                        {{ $child->title }}
-                                    @else
-                                        <?php
-                                        $child_title_i18n = $child->title;
-                                        ?>
-                                        @foreach($child['many'] as $child_page_i18n)
+                        <ul>
+                            @foreach($page->children as $child)
+                                <li>
+                                    <?php
+                                    $target_child = "";
+                                    if (!$page->type) {
+                                        $target_child = "_blank";
+                                    }
+                                    ?>
+                                    <a href="{{ $url = route('pages.id', ['id' => $page->id]) }}"
+                                       target="{{$target_child}}">
+                                        @if(Cookie::get('language')==0)
+                                            {{ $child->title }}
+                                        @else
                                             <?php
-                                            if ($child_page_i18n->language == Cookie::get('language') && $child_page_i18n->title != null) {
-                                                $child_title_i18n = $child_page_i18n->title;
-                                            }
+                                            $child_title_i18n = $child->title;
                                             ?>
-                                        @endforeach
-                                        {{ $child_title_i18n }}
-                                    @endif
-                                </a>
-                            </ul>
-                        @endforeach
-
+                                            @foreach($child['many'] as $child_page_i18n)
+                                                <?php
+                                                if ($child_page_i18n->language == Cookie::get('language') && $child_page_i18n->title != null) {
+                                                    $child_title_i18n = $child_page_i18n->title;
+                                                }
+                                                ?>
+                                            @endforeach
+                                            {{ $child_title_i18n }}
+                                        @endif
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
                     </li>
                 @endforeach
             </ul>
         </div>
 
         <div class="menu_box_select">
-        <select onChange="menu_box_select_chg(this);">
-        <option value="{{ route('index') }}" selected>首頁</option>
+            <select onChange="menu_box_select_chg(this);">
+                <option value="{{ route('index') }}" selected>首頁</option>
 
-        @if(count($pages) > 0)
+                @if(count($pages) > 0)
 
-        @for( $i = 0; $i < count($pages); $i++)
+                    @for( $i = 0; $i < count($pages); $i++)
 
-        @if($pages[$i]->type)
+                        @if($pages[$i]->type)
 
-        <option value="{{ $url = route('pages.id', ['id' => $pages[$i]->id]) }}">{{ $pages[$i]->title }}</option>
+                            <option value="{{ $url = route('pages.id', ['id' => $pages[$i]->id]) }}">{{ $pages[$i]->title }}</option>
 
-        @else
+                        @else
 
-        <option value="{{ $url = route('pages.id', ['id' => $pages[$i]->id, 'type' => $pages[$i]->type ]) }}">{{ $pages[$i]->title }}</option>
+                            <option value="{{ $url = route('pages.id', ['id' => $pages[$i]->id, 'type' => $pages[$i]->type ]) }}">{{ $pages[$i]->title }}</option>
 
-        @endif
+                        @endif
 
 
-        @endfor
+                    @endfor
 
-        @endif
+                @endif
 
-        </select>
+            </select>
         </div>
     </div>
 </div>
