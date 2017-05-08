b2df3604d0be8f72da3724d16a4dff1abb2f1b44
mingburnu <program.developer@hotmail.com>
2017-05-08 15:32:36 +0800

    Signed-off-by: mingburnu <program.developer@hotmail.com>

--- medportal/resources/views/pages.blade.php
+++ medportal/resources/views/pages.blade.php
@@ -46,15 +46,43 @@
     </div>
     <!-- 麵包屑 區塊 End -->
 
-
     <!-- 主內容 區塊 Begin -->
     <div class="news_detail">
         <div class="innerwrapper">
             <!-- 內容 區塊 Begin -->
-
-            <div class="title">{{ $pages_data->title }}</div>
+            <div class="title">
+                @if(Cookie::get('language')==0)
+                    {{ $pages_data->title }}
+                @else
+                    <?php
+                    $title_i18n = $pages_data->title;
+                    ?>
+                    @foreach($pages_data->menupage_i18ns as $page_i18n)
+                        <?php
+                        if ($page_i18n->language == Cookie::get('language') && $page_i18n->title != null) {
+                            $title_i18n = $page_i18n->title;
+                        }
+                        ?>
+                    @endforeach
+                    {{ $title_i18n }}
+                @endif
+            </div>
             <div class="ck_htmlcode">
-                {!! $pages_data->content !!}
+                @if(Cookie::get('language')==0)
+                    {!! $pages_data->content !!}
+                @else
+                    <?php
+                    $content_i18n = $pages_data->content;
+                    ?>
+                    @foreach($pages_data->menupage_i18ns as $page_i18n)
+                        <?php
+                        if ($page_i18n->language == Cookie::get('language') && $page_i18n->content != null) {
+                            $content_i18n = $page_i18n->content;
+                        }
+                        ?>
+                    @endforeach
+                    {!! $content_i18n !!}
+                @endif
             </div>
             <!-- 內容 區塊 End -->
         </div>
